ca35a2c6e0fa4731c2d00c3d7be83536814e3df6
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2023-05-17 22:30:31 -0700

    Implement some methods and their tests for Game class

--- classes/Game.rb
+++ classes/Game.rb
@@ -1,20 +1,81 @@
 class Game
+  @@guesses_left_to_hangman = {
+    0 => [
+      "  ____\n",
+      " |    |\n",
+      "_O_   |\n",
+      " |    |\n",
+      "/ \\   |\n",
+      "  ____|____\n"
+    ],
+    1 => [
+      "  ____\n",
+      " |    |\n",
+      "_O_   |\n",
+      " |    |\n",
+      "/     |\n",
+      "  ____|____\n"
+    ],
+    2 => [
+      "  ____\n",
+      " |    |\n",
+      "_O_   |\n",
+      " |    |\n",
+      "      |\n",
+      "  ____|____\n"
+    ],
+    3 => [
+      "  ____\n",
+      " |    |\n",
+      "_O    |\n",
+      " |    |\n",
+      "      |\n",
+      "  ____|____\n"
+    ],
+    4 => [
+      "  ____\n",
+      " |    |\n",
+      " O    |\n",
+      " |    |\n",
+      "      |\n",
+      "  ____|____\n"
+    ],
+    5 => [
+      "  ____\n",
+      " |    |\n",
+      " O    |\n",
+      "      |\n",
+      "      |\n",
+      "  ____|____\n"
+    ],
+    6 => [
+      "  ____\n",
+      " |    |\n",
+      "      |\n",
+      "      |\n",
+      "      |\n",
+      "  ____|____\n"
+    ]
+  }
+
   def initialize(words_list_class)
-    @words_list_class = words_list_class
+    @words_list = words_list_class
     @min_word_size = 5
     @max_word_size = 12
     @word = ""
     @max_wrong_guesses = 6
+    @wrong_guesses = 0
     @wrong_char_guesses = Set.new
     @correct_char_guesses = Set.new
     @word_guesses = []
   end
 
-  attr_reader(:words_list_class)
+  attr_reader(:words_list)
   attr_reader(:min_word_size)
   attr_reader(:max_word_size)
   attr_accessor(:word)
   attr_reader(:max_wrong_guesses)
+  attr_accessor(:wrong_guesses)
   attr_accessor(:wrong_char_guesses)
   attr_accessor(:correct_char_guesses)
   attr_accessor(:word_guesses)
@@ -28,31 +89,54 @@ class Game
   end
 
   def get_masked_word
-    # TODO
+    res = []
+    @word.each_char do |char|
+      is_guessed = @correct_char_guesses.include?(char)
+      res.push(is_guessed ? char.upcase : "_")
+    end
+    res
+  end
+
+  def is_alpha_char?(char)
+    char.downcase >= 'a' && char.downcase <= 'z'
+  end
+
+  def is_valid_char_guess?(char)
+    char.class == String && char.size == 1 && is_alpha_char?(char)
+  end
+
+  def is_valid_word_guess?(word)
+    return false if word.class != String
+    return false if word.size < @min_word_size
+    return false if word.size > @max_word_size
+    word.each_char do |char|
+      return false unless is_alpha_char?(char)
+    end
+    true
+  end
+
+  # TODO - write tests for
+  def did_guesser_lose?
+    # wrong_word_guesses = @word_guesses.select { |w| w != @word }
+    # guesses = @wrong_char_guesses.size + wrong_word_guesses.size
+    # guesses >= @max_wrong_guesses
+    @wrong_guesses >= @max_wrong_guesses
+  end
+
+  # TODO - write tests for
+  def did_guesser_win?
+    return true if @correct_char_guesses.size == Set.new(@word.split(''))
+    @word_guesses.size > 0 && word_guesses[-1] == @word
   end
 
   def get_random_word
-    # TODO
-  end
-
-  def did_guesser_lose?
-    # TODO
-  end
-
-  def is_valid_char_guess?
-    # TODO
-  end
-
-  def is_valid_word_guess?
-    # TODO
+    words = @words_list.get_list([@min_word_size, @max_word_size])
+    random_pos = Random.new.rand(0..words.size - 1)
+    words[random_pos]
   end
 
   def get_guesser_input
-    # TODO
-  end
-
-  def did_guesser_win?
-    # TODO
+    # TODO - to test manually
   end
 
   def print_hangman
--- specs/Game.spec.rb
+++ specs/Game.spec.rb
@@ -11,5 +11,132 @@ RSpec.describe "Game" do
     end
   end
 
+  describe "is_word_set?" do
+    it "returns false if called and there is no word selected" do
+      game = Game.new(WordsList)
+      expect(game.is_word_set?).to eq(false)
+    end
 
+    it "returns false if called and the word set does not meet the valid range size" do
+      game = Game.new(WordsList)
+      game.word = "abc"
+      expect(game.is_word_set?).to eq(false)
+    end
+
+    it "returns true if called and the word set meets the valid range size" do
+      game = Game.new(WordsList)
+      game.word = "words"
+      expect(game.is_word_set?).to eq(true)
+    end
+  end
+
+  describe "get_masked_word" do
+    it "returns an array whose size equals the size of the selected word" do
+      game = Game.new(WordsList)
+      game.word = "words"
+      expect(game.get_masked_word.size).to eq(game.word.size)
+    end
+
+    it "returns an array of underscores if the guesser has not guessed any valid single chars yet" do
+      game = Game.new(WordsList)
+      game.word = "words"
+      expected = ['_', '_', '_', '_', '_']
+      expect(game.get_masked_word).to eq(expected)
+    end
+
+    it "returns an array with uppercase alphabet chars for revealed chars that a guesser has guessed correctly for" do
+      game = Game.new(WordsList)
+      game.word = "banana"
+      game.correct_char_guesses = Set.new(['a'])
+      expected = ['_', 'A', '_', 'A', '_', 'A']
+      expect(game.get_masked_word).to eq(expected)
+    end
+
+    it "returns an array with only uppercase alphabet chars if the guesser has guessed correctly all of the word's chars" do
+      game = Game.new(WordsList)
+      game.word = "banana"
+      game.correct_char_guesses = Set.new(['a', 'b', 'n'])
+      expected = ['B', 'A', 'N', 'A', 'N', 'A']
+      expect(game.get_masked_word).to eq(expected)
+    end
+  end
+
+  describe "is_alpha_char?" do
+    it "returns false if called with ''" do
+      game = Game.new(WordsList)
+      expect(game.is_alpha_char?('')).to eq(false)
+    end
+
+    it "returns false if called with '2'" do
+      game = Game.new(WordsList)
+      expect(game.is_alpha_char?('2')).to eq(false)
+    end
+
+    it "returns true if called with 'a'" do
+      game = Game.new(WordsList)
+      expect(game.is_alpha_char?('a')).to eq(true)
+    end
+
+    it "returns true if called with 'Z'" do
+      game = Game.new(WordsList)
+      expect(game.is_alpha_char?('Z')).to eq(true)
+    end
+
+    it "returns true if called with 'G'" do
+      game = Game.new(WordsList)
+      expect(game.is_alpha_char?('G')).to eq(true)
+    end
+  end
+
+  describe "is_valid_char_guess?" do
+    it "returns false if called with 'ads'" do
+      game = Game.new(WordsList)
+      expect(game.is_valid_char_guess?('ads')).to eq(false)
+    end
+
+    it "returns false if called with ''" do
+      game = Game.new(WordsList)
+      expect(game.is_valid_char_guess?('')).to eq(false)
+    end
+
+    it "returns true if called with 'H'" do
+      game = Game.new(WordsList)
+      expect(game.is_valid_char_guess?('H')).to eq(true)
+    end
+  end
+
+  describe "is_valid_word_guess?" do
+    it "returns false if called with 'was'" do
+      game = Game.new(WordsList)
+      expect(game.is_valid_word_guess?('was')).to eq(false)
+    end
+
+    it "returns false if called with 'supermegalongword'" do
+      game = Game.new(WordsList)
+      expect(game.is_valid_word_guess?('supermegalongword')).to eq(false)
+    end
+
+    it "returns false if called with 'badword!'" do
+      game = Game.new(WordsList)
+      expect(game.is_valid_word_guess?('badword!')).to eq(false)
+    end
+
+    it "returns true if called with 'excellent'" do
+      game = Game.new(WordsList)
+      expect(game.is_valid_word_guess?('excellent')).to eq(true)
+    end
+  end
+
+  # TODO - various missing tests
+
+  describe "get_random_word" do
+    it "returns a random English word with only alphabet chars and of a valid size" do
+      game = Game.new(WordsList)
+      res = game.get_random_word
+      expect(res.size).to be_between(game.min_word_size, game.max_word_size)
+      res.each_char do |char|
+        expect(char).to be_between('a', 'z')
+      end
+    end
+  end
 end
