33e15bb385b45235f4ced3b3b5291bcb86a4a736
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2023-05-19 16:02:47 -0700

    Remove dummy test file, add boilerplate starter code for GameSave class and its test file

---
+++ classes/GameSave.rb
@@ -0,0 +1,25 @@
+class GameSave
+  def initialize(saves_path = "../saves")
+    # TODO
+  end
+
+  def does_exist?(save_name)
+    # TODO
+  end
+
+  def count
+    # TODO
+  end
+
+  def create(game_state)
+    # TODO
+  end
+
+  def open(save_name)
+    # TODO
+  end
+
+  get_name_list(limit = 0)
+    # TODO
+  end
+end
---
+++ specs/GameSave.spec.rb
@@ -0,0 +1,10 @@
+require_relative 'spec_helper'
+require_relative '../classes/GameSave'
+
+RSpec.describe "GameSave Class" do
+  describe "TODO" do
+    it "TODO" do
+      # TODO
+    end
+  end
+end
