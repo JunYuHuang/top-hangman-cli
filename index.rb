a9550a8e3bb5c41b9da6d692e542aa6f572474a7
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2023-05-23 12:13:10 -0700

    Update index.rb to work with the updated classes

--- index.rb
+++ index.rb
@@ -1,5 +1,7 @@
 require_relative 'classes/Game'
 require_relative 'classes/WordsList'
+require_relative 'classes/GameSave'
 
 game = Game.new(WordsList)
+game.use_game_save(GameSave.new("saves"))
 game.play
